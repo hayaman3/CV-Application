9a72d995c7865be2091aac0199586a7efe71d1ce
hayaman3 <kevin.haya@yahoo.com>
2022-11-26 23:13:43 +0800

    Added experience.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,49 +5,63 @@ import Education from './Education';
 
 const Form = () => {
   const [page, setPage] = useState("Personal");
+  const [counter, setCounter] = useState(0);
+
   const pageArray = ["Personal","Education","Experience"]
 
+  const ENUM_STATES = {
+    0: <Personal />,
+    1: <Education />,
+    // 3: <Experience />,
+  };
+  
+  const EnumState = () => {
+    console.log(ENUM_STATES[counter]+ "asd")
+    return ENUM_STATES[counter]
+  } 
+
   const goBackPage = () =>{
-    switch(page) {
-      case "Personal":
-        // code block
-        break;
-      case "Education":
-        setPage("Personal")
-        break;
-      default:
-        // code block
-    }
+    // switch(page) {
+    //   case "Personal":
+    //     // code block
+    //     break;
+    //   case "Education":
+    //     setPage("Personal")
+    //     break;
+    //   default:
+    //     // code block
+    // }
+    setCounter(counter -1)
+
 
   };
 
   const goNextPage = () =>{
-    switch(page) {
-      case "Personal":
-        setPage("Education")
-        // code block
-        break;
-      case "Education":
+    // switch(page) {
+    //   case "Personal":
+    //     setPage("Education")
+    //     break;
+    //   case "Education":
         
-        break;
-      default:
-        // code block
-    }
-  };
+    //     break;
+    //   default:
+    //     // code block
+    // }
+    setCounter(counter +1)
 
+  };
 
 
   return (
     // <form>
     <div>
-      {page === "Personal" &&
-        <Personal />
-      }
-      {page === "Education" &&
-        <Education />
-      }
-      <button onClick={goBackPage}>Back</button>
-      <button onClick={goNextPage}>Next</button>
+      {EnumState({counter})}
+      <div className='form-nav'>
+        {counter !== 0 &&
+          <button id="back-button" onClick={goBackPage}>Back</button>
+        }
+        <button onClick={goNextPage}>Next</button>
+      </div>
     </div>
     // </form>
   );
